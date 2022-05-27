e0ba2d90fbfed37e1fb3bd2ae541f46838ac0aec
forexel <danick@inbox.ru>
2022-05-27 13:33:40 +0300

    Удалить все данные

--- main.go
+++
@@ -1,8 +0,0 @@
-package main // говорим, что файл принадлежит пакету main
-
-import "fmt"
-
-// func -- объявление функции
-func main() { // func main() -- точка входа
-	fmt.Println("Hello, world !") // println -- функция, выводящая в консоль строку
-}
